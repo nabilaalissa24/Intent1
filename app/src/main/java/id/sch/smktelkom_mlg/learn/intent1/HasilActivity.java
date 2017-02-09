4dd98d617ffc396bfcc29109fbaae994d5d12899
nabilaalissa24 <9d.nabilaalissa@gmail.com>
2017-02-09 22:41:19 +0700

    startActivity Part 2

--- app/src/main/java/id/sch/smktelkom_mlg/learn/intent1/HasilActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/learn/intent1/HasilActivity.java
@@ -1,42 +1,41 @@
 package id.sch.smktelkom_mlg.learn.intent1;
 
+import android.icu.util.Calendar;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
-import android.view.View;
+import android.widget.TextView;
 
 public class HasilActivity extends AppCompatActivity {
 
+    +
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_hasil);
-
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-
-
-        @Override
-        public boolean onOptionsItemSelected (MenuItem item){
-            if (item.getItemId() == android.R.id.home) {
-                onBackPressed();
-                return true;
-            }
-            return super.onOptionsItemSelected(item);
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == android.R.id.home) {
+            public boolean onOptionsItemSelected (MenuItem item){
+                return super.onOptionsItemSelected(item);
             }
 
-        @Override
-        protected void onCreate (Bundle savedInstanceState){
-            super.onCreate(savedInstanceState);
-            setContentView(R.layout.activity_hasil);
 
-            findViewById(R.id.buttonBack).setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View view) {
-                    onBackPressed();
-                }
-            });
-            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+            @Override
+            protected void onCreate (Bundle savedInstanceState){
+                super.onCreate(savedInstanceState);
+                setContentView(R.layout.activity_hasil);
+
+                String nama = getIntent().getStringExtra(MainActivity.NAMA);
+                int umur = getIntent().getIntExtra(MainActivity.UMUR, 0);
+
+                int yearNow = Calendar.getInstance().get(Calendar.YEAR);
+                int tahunLahir = yearNow - umur;
+
+                TextView tvHasil = (TextView) findViewById(R.id.textViewHasil);
+                tvHasil.setText(nama + " lahir pada tahun " + tahunLahir);
+
+                getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
 
+            }
+        }
     }
 }
